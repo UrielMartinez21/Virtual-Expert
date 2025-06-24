f96cb4508da357c05cc5c1d2807c620c7317b560
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-06-24 08:01:38 -0600

    feat: added new field in Expert class

--- experts/models.py
+++ experts/models.py
@@ -7,6 +7,7 @@ class Expert(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name} ({self.profile.user.username})"
