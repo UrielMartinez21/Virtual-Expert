26e2f7ce42ac76caba259fc6890aafccd98f4630
UrielMartinez21 <uriellmartinezz35@gmail.com>
2025-06-26 08:01:47 -0600

    chore: added a comment

--- experts/urls.py
+++ experts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.my_virtual_experts, name='my_virtual_experts'),
+
+    # to create virtual expert
     path('create_virtual_expert/', views.create_virtual_expert, name='create_virtual_expert'),
     path('send_data_to_expert/', views.send_data_to_expert, name='send_data_to_expert'),
 
